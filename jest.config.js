e12a42b789828d352733267276f68a2b03c40d25
unional <homawong@gmail.com>
2022-05-15 02:57:02 -0700

    chore: update project

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,20 @@
-const common = require('@unional/devpkg-node/simple/config/jest.common')
-module.exports = Object.assign(common, {
-  'testEnvironment': 'node'
-})
+module.exports = {
+  'collectCoverageFrom': [
+    '<rootDir>/ts/**/*.[jt]s'
+  ],
+  'roots': [
+    '<rootDir>/ts',
+  ],
+  'testMatch': ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
+  'watchPlugins': [
+    'jest-watch-suspend',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    [
+      'jest-watch-toggle-config', { 'setting': 'verbose' },
+    ],
+    [
+      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
+    ],
+  ],
+}
